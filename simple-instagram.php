d40c5e7a82425c2a84084b22612a268c3ac364bc
Aaron Speer <aaron@westwerk.com>
2013-12-31 10:58:35 -0600

    Remove TODO from files

--- simple-instagram.php
+++ simple-instagram.php
@@ -13,7 +13,6 @@
  *
  * @wordpress-plugin
  * Plugin Name:       Simple Instagram
- * Plugin URI:        @TODO
  * Description:       A plugin to allow users to include InstaGram feeds, media, and information.
  * Version:           1.0.0
  * Author:            Aaron Speer
@@ -35,9 +34,6 @@ if ( ! defined( 'WPINC' ) ) {
  *----------------------------------------------------------------------------*/
 
 /*
- * @TODO:
- *
- * - replace `class-plugin-name.php` with the name of the plugin's class file
  *
  */
 require_once( plugin_dir_path( __FILE__ ) . 'public/simple-instagram.php' );
@@ -53,19 +49,11 @@ require_once( plugin_dir_path( __FILE__ ) . 'includes/simple-instagram-widgets.p
  * Register hooks that are fired when the plugin is activated or deactivated.
  * When the plugin is deleted, the uninstall.php file is loaded.
  *
- * @TODO:
- *
- * - replace Plugin_Name with the name of the class defined in
- *   `class-plugin-name.php`
  */
 register_activation_hook( __FILE__, array( 'simpleInstagram', 'activate' ) );
 register_deactivation_hook( __FILE__, array( 'simpleInstagram', 'deactivate' ) );
 
 /*
- * @TODO:
- *
- * - replace Plugin_Name with the name of the class defined in
- *   `class-plugin-name.php`
  */
 add_action( 'plugins_loaded', array( 'simpleInstagram', 'get_instance' ) );
 
@@ -157,11 +145,6 @@ function register_instagram(){
  *----------------------------------------------------------------------------*/
 
 /*
- * @TODO:
- *
- * - replace `class-plugin-admin.php` with the name of the plugin's admin file
- * - replace Plugin_Name_Admin with the name of the class defined in
- *   `class-plugin-name-admin.php`
  *
  * If you want to include Ajax within the dashboard, change the following
  * conditional to:
